7aa7c49c8d90eed6f3ec6ef7927092ce5756e4c9
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-11-03 10:19:56 +0400

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,64 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// import { defineConfig, globalIgnores } from "eslint/config";
+// import nextVitals from "eslint-config-next/core-web-vitals";
+// import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+// const eslintConfig = defineConfig([
+//   ...nextVitals,
+//   ...nextTs,
+//   // Override default ignores of eslint-config-next.
+//   globalIgnores([
+//     // Default ignores of eslint-config-next:
+//     ".next/**",
+//     "out/**",
+//     "build/**",
+//     "next-env.d.ts",
+//   ]),
+// ]);
+
+// export default eslintConfig;
+import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "eslint:recommended",
+      "plugin:react/recommended",
+    ],
+    rules: {
+      "no-unused-vars": "error", // Disallows unused variables and functions
+      "no-console": "error", // Disallows console.log statements
+      "prefer-const": "error", // Disallows let if variables are not reassigned
+      quotes: ["error", "double"], // Enforces the use of double quotes over single quotes
+      semi: ["error", "always"], // Enforces semicolons at the end of statements
+      "no-multiple-empty-lines": ["error", { max: 1 }], // Disallows multiple empty lines
+      "no-extra-semi": "error", // Disallows unnecessary semicolons
+      "react/no-array-index-key": "warn",
+      "react/react-in-jsx-scope": "off",
+      "no-undef": "off",
+      camelcase: "error", // Enforces camelCase naming convention
+    },
+
+    plugins: ["react"],
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }),
+];
 
 export default eslintConfig;
